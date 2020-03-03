c2faa7c81c427659c13306c2148b91adf22bf066
KENNEDY <s-cokennedy@lwsd.org>
2020-03-03 10:46:26 -0800

    Add project files.

---
+++ WumpusTest/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wumpus
+{
+    public partial class Form1 : Form
+    {
+
+        private GameObject _GameObject;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConstructorButton_Click(object sender, EventArgs e)
+        {
+            _GameObject = new GameObject();
+        }
+    }
+}
---
+++ WumpusTest/GameObject.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wumpus
+{
+    class GameObject
+    {
+        private Map _Map;
+        private Cave _Cave;
+        private Player _Player;
+
+        private String State;
+
+        public GameObject()
+        {
+            _Cave = new Cave();
+            _Map = new Map();
+            _Player = new Player();
+            State = "Not Started";
+        }
+
+        public void MoveForward(int direction)
+        {
+            int currentRoom = _Map.GetRoom();
+            int nextRoom = _Cave.GetNextRoom(currentRoom, direction);
+            if (nextRoom != 0)
+            {
+                _Player.AddGold(1);
+            }
+            else
+            {
+                // can't move that way
+            }
+        }
+
+        public void StartGame()
+        {
+            // create new cave
+            // move player to new location
+            // create the obstacles
+            State = "Playing";
+        }
+
+        public void SeeHighScores()
+        {
+            State = "Viewing High Scores";
+        }
+
+        public void BuyArrows()
+        {
+            //TODO: ask trivia for questions and display them
+            bool success = true;
+            if (success)
+            {
+                _Player.AddArrows(5);
+            }
+        }
+
+        public void EndGame()
+        {
+            State = "Ended";
+        }
+
+        public void StartTrivia()
+        {
+            State = "Trivia";
+        }
+
+        public void EndTrivia()
+        {
+            State = "Playing";
+        }
+
+        public void AnswerTrivia()
+        {
+            _Player.RemoveGold(1);
+        }
+
+    }
+}
