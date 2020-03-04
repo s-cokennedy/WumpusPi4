591a16cf9252fff0e92f10e41673d6b1df48ff01
s-cokennedy <s-cokennedy@lwsd.org>
2020-03-04 10:11:43 -0800

    Add Trivia and stubs, rename namespace

---
+++ WumpusTest/Trivia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wumpus
+{
+    class Trivia
+    {
+        public Trivia()
+        {
+
+        }
+
+        public void ReadQuestionsFromFile()
+        {
+
+        }
+
+        public String GetQuestion()
+        {
+            return "What is 1 + 1?";
+        }
+    }
+}
